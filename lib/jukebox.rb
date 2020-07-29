d03fc833a318df972c22f89df785df04e7a05f9c
Jarrell Jackson <jdjack3@gmail.com>
2020-07-29 20:01:04 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,10 @@
-# Add your code here
+require 'pry'
+
+
+
+
+
+#The help command should output instructions for the user on how to use the jukebox.
+#The list command should output a list of songs that the user can play.
+#The play command should ask a user to input a song name or track number. It should then output the appropriate song name.
+#If the user types exit, the jukebox should say goodbye and the program should shut down.
